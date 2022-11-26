182c2fc305dfc68a639a3575108158241bba04a6
syn4 <ranvirdheria@gmail.com>
2022-11-25 21:26:55 -0500

    crash course

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// // // Loops 
+
+// let count = 1;
+
+// while (count <= 100){
+// console.log(count)
+// count = count +1;
+// }
+
+
+
+
+for (let i = 1;i <= 20; ++i){
+        if (i % 5 === 0 && i % 3 === 0){
+        console.log(`${i} -->Frontend Simplified`)
+    }
+else if (i % 3 === 0){
+        console.log(`${i} --> frontend`)
+    }
+else if (i % 5 === 0){
+        console.log(`${i} -->simplified`)
+    }
+else
+console.log(`${i} --> ${i}`)
+}
+
+
+    
+    
+    
